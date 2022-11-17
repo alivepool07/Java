e0318ae1575372cc07f63aeaa9f3878a50631992
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2022-11-17 20:07:52 +0530

    Update Linear_search.java

--- java_basics/Linear_search.java
+++ java_basics/Linear_search.java
@@ -6,17 +6,17 @@ public class Linear_search {
         System.out.print("Target is present at ");
         System.out.println(ans);
     }
-    static int linear_search(int[] arr,int target){
-        if (arr.length == 0){
+    static int linear_search(int[] arr,int target){         
+        if (arr.length == 0){                               //Checking if the list is empty by any chance.
             System.out.println("Empty list");
         }
 
-        for (int i = 0 ; i < arr.length ; i++){
-            int temp = arr[i];
+        for (int i = 0 ; i < arr.length ; i++){            //Initialising a loop for traversing over the whole list.
+            int temp = arr[i];                             //Creating a temp var to store the index value so that it could be passed if value at that index equals target value.
             if (temp == target ){
-                return i;
+                return i;                                 //Returning the index value. **HERE RETURNING TEMP VALUE WOULD HAVE GIVEN VALUE AT THAT INDEX
             }
         }
-        return -1;
+        return -1;                                        //INDICATES TARGET IS PRESENT NO WHERE IN THE LIST.
     }
 }
