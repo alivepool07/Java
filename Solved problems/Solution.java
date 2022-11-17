4eb8d429a8ecd0ffcd8b84eccbcf6ae0a2a66554
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2022-11-18 00:40:46 +0530

    Update Solution.java

--- Solved problems/Solution.java
+++ Solved problems/Solution.java
@@ -12,29 +12,29 @@
 
 public class Solution {
     public int findNumbers(int[] nums) {
-        int count = 0;
-        for(int num : nums){
-            if(even(num)){                  
+        int count = 0;                  
+        for(int num : nums){                    // Loop initialisation
+            if(even(num)){                      // calling even function to check every digit of nums .
                 count++;
             }
         }
         return count;
     }
-    boolean even(int num){
-        int get_num = digit(num);
+    boolean even(int num){              
+        int get_num = digit(num);               // Calling digit function 
 
-        return get_num % 2 ==0;
+        return get_num % 2 ==0;                 // Returns true for every num that has even nbumber of digits
     }
     int digit(int num){
-        if (num == 0){
+        if (num == 0){                          // boundary condition : as loop won;t execute for num = 0 ,still it will have 1 digit .
             return 1;
         }
-        if (num < 0){
-            num= num * -1;
+        if (num < 0){                           // Boundary condition : When given a negative interger value , this line will convert it into +ve for counting digits
+            num= num * -1;                      // Otherwiase , it'll return -1
         }
         int count = 0;
 
-        while(num > 0){
+        while(num > 0){                        // checking the total number of digits in a given interger by dividing it by 10 
             count++;
             num = num/10;
         }
