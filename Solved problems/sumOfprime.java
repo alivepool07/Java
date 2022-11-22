4359955a955633865161906394f509507c204a7b
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2022-11-22 16:10:23 +0530

    Update sumOfprime.java

--- Solved problems/sumOfprime.java
+++ Solved problems/sumOfprime.java
@@ -1,6 +1,11 @@
 // Given a number N. Find if it can be expressed as sum of two prime numbers.
 
-
+/* Logic : Any +ve interger less than 4  can't be expressed as a sum of 2 prime numbers , in that case  return "NO"
+           
+           Any EVEN number greater than 4 can be expressed as sum of two prime numbers , return Yes in that case.
+           
+           For any ODD value of N , if N-2 is prime , return Yes 
+*/
 
 public class sumOfprime {
     public static void main(String[] args) {
