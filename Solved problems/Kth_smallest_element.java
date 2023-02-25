ecc13cfc7506bdb1770600111b80cc37acf7f70b
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2023-02-25 15:34:44 +0530

    Create Kth_smallest_element.java

---
+++ Solved problems/Kth_smallest_element.java
@@ -0,0 +1,54 @@
+/*
+Given an array arr[] and an integer K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. 
+It is given that all array elements are distinct.
+
+Note :-  l and r denotes the starting and ending index of the array.
+*/
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+		out.flush();
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        //Your code here 
+        Arrays.sort(arr); 
+        
+    
+    return arr[k-1];
+}
+    
+}
