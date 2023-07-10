f0d9685b59680917f9c7ff2599044497da0f2fda
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2023-07-10 21:39:05 +0530

    Create Quick sort.java

---
+++ Solved problems/Quick sort.java
@@ -0,0 +1,79 @@
+/*
+Quick Sort is a Divide and Conquer algorithm. It picks an element as a pivot and partitions the given array around the picked pivot.
+Given an array arr[], its starting position is low (the index of the array) and its ending position is high(the index of the array).
+
+Note: The low and high are inclusive.
+
+Implement the partition() and quickSort() functions to sort the array.Quick Sort is a Divide and Conquer algorithm. It picks an element as a pivot and partitions the given array around the picked pivot.
+Given an array arr[], its starting position is low (the index of the array) and its ending position is high(the index of the array).
+
+Note: The low and high are inclusive.
+
+Implement the partition() and quickSort() functions to sort the array.
+*/
+
+//{ Driver Code Starts
+import java.util.*;
+class Sorting
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    
+    // Driver program
+    public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			
+			new Solution().quickSort(arr,0,n-1);
+			printArray(arr);
+		    T--;
+		}
+} }
+// } Driver Code Ends
+
+
+class Solution
+{
+    // chl suru krte hai
+    static void quickSort(int arr[], int low, int high)
+    {
+        if(low<high){
+            int p=partition(arr,low,high);
+            quickSort(arr,low,p-1);
+            quickSort(arr,p+1,high);
+        }
+        
+    }
+    static int partition(int arr[], int low, int high)
+    {
+        int pivot=arr[high];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j]<pivot){
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        i++;
+        swap(arr,i,high);
+        return i;
+        }
+       static void swap(int arr[],int a,int b){
+            int temp=arr[a];
+            arr[a]=arr[b];
+            arr[b]=temp;
+        }
+}
